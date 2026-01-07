6df42563452f6de7ace80751d79f409181387a58
Sohaib-khaled <80972621+sokhsa96@users.noreply.github.com>
2026-01-07 13:35:50 +0300

    sddd

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,11 +5,9 @@ const api = axios.create({
   baseURL: 'https://fakestoreapi.com',
   headers: {
     'Content-Type': 'application/json',
+    // Add this line:
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
   },
-});
-
-api.interceptors.request.use((config) => {
-  return config;
 });
 
 export default api;
